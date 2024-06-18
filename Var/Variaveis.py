39a00b484e13a0a1ba7dcfde2420cedd057fe99d
Vaalls <bielvalls4@gmail.com.br>
2024-06-18 12:21:38 -0300

    Aprendendo a manipular e saber diferentes variaveis em python.

---
+++ Var/Variaveis.py
@@ -0,0 +1,13 @@
+nome = input("Digite seu nome ")
+empresa = input("Digite o nome da empresa ")
+qtd_Funcionarios = 10
+mediaMensalidade = input("Digite a média da mensalidade ")
+
+print(nome + " Trabalha na empresa: " + empresa )
+print("Possui :", qtd_Funcionarios, " em sua empresa")
+print("A média da mensalidade é de: " , qtd_Funcionarios)
+print("********************Verifique os tipos de dados abaixo***********************")
+print("O tipo de dado da variavel [nome] é: " , type(nome))
+print("O tipo de dado da variavel [empresa] é: " , type(empresa))
+print("O tipo de dado da variavel [quantidade de fun] é: " , type(qtd_Funcionarios))
+print("O tipo de dado da variavel [mensalidade] é: " , type(mediaMensalidade))
