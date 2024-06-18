80cbf53e2cd5a4bf50944099cc859c7f43a39696
Vaalls <bielvalls4@gmail.com.br>
2024-06-18 12:08:29 -0300

    Identificando funções para manipular textos.

---
+++ ManipulacaoArquivos/Textos.py
@@ -0,0 +1,4 @@
+texto = "Gabriel e Beatriz"
+        #01234567891123456
+print(texto[0:7])
+print(texto[10:])
---
+++ ManipulacaoArquivos/encontrarEConquistar.py
@@ -0,0 +1,4 @@
+texto = "Eu amo Java!"
+
+print(texto.find("a"))
+print(texto.split(" "))
